b8274ffd6692f730f32ece0a006439a3e294a114
Jonas Jermann <jjermann2@gmail.com>
2017-10-28 17:46:09 +0200

    remove semicolons

--- densities.py
+++ densities.py
@@ -14,19 +14,19 @@ def eval_expr(expr):
 
 def eval_(node):
     if isinstance(node, ast.Num):
-      return ConstantDensity(node.n);
+      return ConstantDensity(node.n)
     elif isinstance(node, ast.Name):
-      nodeStr = node.id;
-      match = re.search(r'(m(\d*))?d(\d+)', nodeStr);
+      nodeStr = node.id
+      match = re.search(r'(m(\d*))?d(\d+)', nodeStr)
       if match is None:
-        raise TypeError(node);
+        raise TypeError(node)
       else:
         if match.group(2):
-          nr = int(match.group(2));
+          nr = int(match.group(2))
         else:
-          nr = 1;
-        density = DieDensity(int(match.group(3)));
-        return density.arithMult(nr);
+          nr = 1
+        density = DieDensity(int(match.group(3)))
+        return density.arithMult(nr)
     elif isinstance(node, ast.BinOp):
         return operators[type(node.op)](eval_(node.left), eval_(node.right))
     elif isinstance(node, ast.UnaryOp):
@@ -36,217 +36,217 @@ def eval_(node):
 
 def getDensity(arg):
   if isinstance(arg, (int, long, float)):
-    return ConstantDensity(arg);
+    return ConstantDensity(arg)
 
   if isinstance(arg, Density):
-    return arg;
+    return arg
   else:
-    raise ValueError("arg must be a Density or a number!");
+    raise ValueError("arg must be a Density or a number!")
 
           
 class Density:
   def __init__(self, densities):
     if isinstance(densities, dict):
-      self.densities = densities;
+      self.densities = densities
     else:
-      raise ValueError("densities must be a density dictionary");
+      raise ValueError("densities must be a density dictionary")
 
   def __str__(self):
-    s = "";
+    s = ""
     if not self.isValid():
-      s += "Invalid Density!\n";
-    s += "{:>12}\t{:>12.5f}".format("Expected", self.expected()) + "\n";
-    s += "{:>12}\t{:>12.5f}".format("Stdev", self.stdev()) + "\n";
-    s += "\n";
-    s += "{:>12}\t{:>12}".format("Result", "Probability") + "\n";
+      s += "Invalid Density!\n"
+    s += "{:>12}\t{:>12.5f}".format("Expected", self.expected()) + "\n"
+    s += "{:>12}\t{:>12.5f}".format("Stdev", self.stdev()) + "\n"
+    s += "\n"
+    s += "{:>12}\t{:>12}".format("Result", "Probability") + "\n"
     for dKey in sorted(self.densities):
-      prob = self.densities[dKey];
+      prob = self.densities[dKey]
       if prob > 0:
-        s += "{:>12}\t{:>12.4%}".format(dKey, prob) + "\n";
-    return s;
+        s += "{:>12}\t{:>12.4%}".format(dKey, prob) + "\n"
+    return s
  
   def isValid(self):
-    return abs(1.0 - sum(self.densities.values())) < 1e-09;
+    return abs(1.0 - sum(self.densities.values())) < 1e-09
 
   def arithMult(self, other):
     if isinstance(other, (int, long)) and other >= 0:
       if other == 0:
-        return ZeroDensity();
+        return ZeroDensity()
       else:
-        res = self;
+        res = self
         for i in range(other-1):
-          res += self;
-        return res;
+          res += self
+        return res
     else:
-      raise ValueError("other must be a nonnegative int or long!");
+      raise ValueError("other must be a nonnegative int or long!")
     
   def binOp(self, other, op):
-    otherDensity = getDensity(other);
-    resDensity = {};
+    otherDensity = getDensity(other)
+    resDensity = {}
     for sKey in self.densities.keys():
       for oKey in otherDensity.densities.keys():
-        resKey = op(sKey, oKey);
+        resKey = op(sKey, oKey)
         if resKey not in resDensity:
-          resDensity[resKey] = 0.0;
-        resDensity[resKey] += 1.0*self.densities[sKey]*otherDensity.densities[oKey];
-    return Density(resDensity);
+          resDensity[resKey] = 0.0
+        resDensity[resKey] += 1.0*self.densities[sKey]*otherDensity.densities[oKey]
+    return Density(resDensity)
 
   def __add__(self, other):
     return self.binOp(other, lambda a,b : a+b)
 
   def __sub__(self, other):
-    return self + (-other);
+    return self + (-other)
   
   def __mul__(self, other):
-    return self.binOp(other, lambda a,b: a*b);
+    return self.binOp(other, lambda a,b: a*b)
 
   __radd__ = __add__
   __rsub__ = __sub__
   __rmul__ = __mul__
 
   def op(self, op):
-    densities = {};
+    densities = {}
     for key in self.densities.keys():
-      opKey = op(key);
+      opKey = op(key)
       if opKey not in densities:
-        densities[opKey] = 0.0;
-      densities[opKey] += self.densities[key];
-    return Density(densities);
+        densities[opKey] = 0.0
+      densities[opKey] += self.densities[key]
+    return Density(densities)
 
   def __neg__(self):
-    return self.op(lambda k: -k);
+    return self.op(lambda k: -k)
 
   def __abs__(self):
-    return self.op(lambda k: abs(k));
+    return self.op(lambda k: abs(k))
 
   def prob(self, other, cond):
-    otherDensity = getDensity(other);
-    sum = 0.0;
+    otherDensity = getDensity(other)
+    sum = 0.0
     for sKey in self.densities.keys():
       for oKey in otherDensity.densities.keys():
         if cond(sKey, oKey):
-          sum += self.densities[sKey]*otherDensity.densities[oKey];
-    return sum;
+          sum += self.densities[sKey]*otherDensity.densities[oKey]
+    return sum
 
   def __eq__(self, y):
-    return self.prob(y, lambda a,b: a == b);
+    return self.prob(y, lambda a,b: a == b)
 
   def __neq__(self, y):
-    return self.prob(y, lambda a,b: a != b);
+    return self.prob(y, lambda a,b: a != b)
 
   def __lt__(self, y):
-    return self.prob(y, lambda a,b: a < b);
+    return self.prob(y, lambda a,b: a < b)
 
   def __le__(self, y):
-    return self.prob(y, lambda a,b: a <= b);
+    return self.prob(y, lambda a,b: a <= b)
 
   def __gt__(self, y):
-    return self.prob(y, lambda a,b: a > b);
+    return self.prob(y, lambda a,b: a > b)
 
   def __ge__(self, y):
-    return self.prob(y, lambda a,b: a >= b);
+    return self.prob(y, lambda a,b: a >= b)
 
   def conditionalDensity(self, cond):
-    densities = {};
+    densities = {}
     for key in self.densities.keys():
       if cond(key):
-        densities[key] = self.densities[key];
-    condProb = sum(densities.values());
+        densities[key] = self.densities[key]
+    condProb = sum(densities.values())
     for key in densities.keys():
-      densities[key] /= condProb;
-    return Density(densities);
+      densities[key] /= condProb
+    return Density(densities)
 
   def expected(self):
-    sum = 0.0;
+    sum = 0.0
     for key in self.densities.keys():
-      sum += key*self.densities[key];
-    return sum;
+      sum += key*self.densities[key]
+    return sum
 
   def variance(self):
-    sum = 0.0;
-    expected = self.expected();
+    sum = 0.0
+    expected = self.expected()
     for key in self.densities.keys():
-      sum += (key-expected)**2*self.densities[key];
-    return sum;
+      sum += (key-expected)**2*self.densities[key]
+    return sum
 
   def stdev(self):
-    return math.sqrt(self.variance());
+    return math.sqrt(self.variance())
 
   def plot(self, width=70):
-    maxPerc = max(self.densities.values());
-    minPerc = maxPerc*0.5/width*0.8;
-    plotRes = "\n";
+    maxPerc = max(self.densities.values())
+    minPerc = maxPerc*0.5/width*0.8
+    plotRes = "\n"
 
     for key in sorted(self.densities):
-      p = self.densities[key];
-      plotRes += "{0:>12}\t".format(key);
-      numberOfBars = int(round(p*width*1.0/maxPerc));
+      p = self.densities[key]
+      plotRes += "{0:>12}\t".format(key)
+      numberOfBars = int(round(p*width*1.0/maxPerc))
       for k in range(1, numberOfBars+1):
-        plotRes += "|";
-      plotRes += "\n";
-    return plotRes;
+        plotRes += "|"
+      plotRes += "\n"
+    return plotRes
 
   def summedDensity(self, goal):
     if min(self.densities.keys()) <= 0:
-      raise ValueError("summedDensity only works with positive results!");
+      raise ValueError("summedDensity only works with positive results!")
 
-    densities = {};
-    prevDensity = ZeroDensity();
-    prevProb = 1;
-    prevKey = 0;
+    densities = {}
+    prevDensity = ZeroDensity()
+    prevProb = 1
+    prevKey = 0
  
     while (prevProb > 0):
-      tDensity = prevDensity.conditionalDensity(lambda k: k <= goal) + self;
-      tProb = tDensity.prob(goal, lambda a,b: a > b)*prevProb;
+      tDensity = prevDensity.conditionalDensity(lambda k: k <= goal) + self
+      tProb = tDensity.prob(goal, lambda a,b: a > b)*prevProb
       if (tProb > 0):
-        densities[prevKey] = tProb;
+        densities[prevKey] = tProb
  
-      prevDensity += self;
-      prevProb = prevDensity.prob(goal, lambda a,b: a <= b);
-      prevKey += 1;
+      prevDensity += self
+      prevProb = prevDensity.prob(goal, lambda a,b: a <= b)
+      prevKey += 1
    
-    return Density(densities);
+    return Density(densities)
  
 
 class DieDensity(Density):
   def __init__(self, die):
-    densities = {};
+    densities = {}
     for r in range(1, die+1):
-      densities[r] = 1.0 / die;
-    Density.__init__(self, densities);
+      densities[r] = 1.0 / die
+    Density.__init__(self, densities)
  
 class ConstantDensity(Density):
   def __init__(self, const):
-    densities = {const:1.0};
-    Density.__init__(self, densities);
+    densities = {const:1.0}
+    Density.__init__(self, densities)
  
 class ZeroDensity(ConstantDensity):
   def __init__(self):
-    ConstantDensity.__init__(self, 0);
+    ConstantDensity.__init__(self, 0)
 
 def AdvantageDieDensity(die):
-  dieDensity = DieDensity(die);
-  return dieDensity.binOp(dieDensity, lambda a,b: max(a,b));
+  dieDensity = DieDensity(die)
+  return dieDensity.binOp(dieDensity, lambda a,b: max(a,b))
 
 def DisadvantageDieDensity(die):
-  dieDensity = DieDensity(die);
-  return dieDensity.binOp(dieDensity, lambda a,b: min(a,b));
+  dieDensity = DieDensity(die)
+  return dieDensity.binOp(dieDensity, lambda a,b: min(a,b))
 
 def DieExpression(expr):
-  return eval_expr(expr);
+  return eval_expr(expr)
 
-d2   = DieDensity(2);
-d3   = DieDensity(3);
-d4   = DieDensity(4);
-d6   = DieDensity(6);
-d8   = DieDensity(8);
-d10  = DieDensity(10);
-d12  = DieDensity(12);
-d20  = DieDensity(20);
-d100 = DieDensity(100);
-ex   = DieExpression("4+d15+m2d5+2");
-ad20 = AdvantageDieDensity(20);
-dd20 = DisadvantageDieDensity(20);
+d2   = DieDensity(2)
+d3   = DieDensity(3)
+d4   = DieDensity(4)
+d6   = DieDensity(6)
+d8   = DieDensity(8)
+d10  = DieDensity(10)
+d12  = DieDensity(12)
+d20  = DieDensity(20)
+d100 = DieDensity(100)
+ex   = DieExpression("4+d15+m2d5+2")
+ad20 = AdvantageDieDensity(20)
+dd20 = DisadvantageDieDensity(20)
 
 #print "{:.4%}".format(d6+d10 <= d8)
 #print d20+d20+4
@@ -255,10 +255,10 @@ dd20 = DisadvantageDieDensity(20);
 #print ad20
 #print dd20
 
-#twod6 = d6.arithMult(2);
-twod6 = d6 + d6;
-turnsToSearch = 50;
-summed = twod6.summedDensity(turnsToSearch);
-print summed;
-print summed.plot();
+#twod6 = d6.arithMult(2)
+twod6 = d6 + d6
+turnsToSearch = 50
+summed = twod6.summedDensity(turnsToSearch)
+print summed
+print summed.plot()
 
